63309fe635fb1d65325639692f82783766ecab2a
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2023-02-11 18:37:48 -0300

    Fix Example

--- MelodyScout_Bot/botFunctions/commands/start.ts
+++ MelodyScout_Bot/botFunctions/commands/start.ts
@@ -18,7 +18,7 @@ export class StartCommand {
       void this.ctxFunctions.reply(ctx, 'Tudo é melhor com amigos, não é mesmo? Crie um grupo com seus amigos e me adicione nele, pode ser um grupo ja criado também o importante e me adicionar nele, prometo que eu sou legal! Em seguida utilize o comando /start lá novamente que eu te ajudarei a me configurar!')
       return
     }
-    await this.ctxFunctions.reply(ctx, 'Que legal fazer parte desse grupo com vocês! Espero que gostem de mim! Bom, vamos lá!\n\nUtilize o comando <code>/myuser</code> para definir seu nome de usuário do Last.fm!\n\nPor exemplo: <code>/track MelodyScout</code>')
+    await this.ctxFunctions.reply(ctx, 'Que legal fazer parte desse grupo com vocês! Espero que gostem de mim! Bom, vamos lá!\n\nUtilize o comando <code>/myuser</code> para definir seu nome de usuário do Last.fm!\n\nPor exemplo: <code>/myuser MelodyScout</code>')
     await this.ctxFunctions.reply(ctx, 'Aproveitando a oportunidade, você pode me enviar o comando /help para saber mais sobre os comandos que eu possuo!')
   }
 }
