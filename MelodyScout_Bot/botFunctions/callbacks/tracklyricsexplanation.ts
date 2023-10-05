65049402b1fdc702407a57054680942a1681a0e3
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2023-10-04 22:23:30 -0300

    Rename file uploads to use track and artist names
    This commit renames the image file uploads to use the track name and artist name in brackets instead of parentheses. This change ensures that the file names are more readable and consistent.

--- MelodyScout_Bot/botFunctions/callbacks/tracklyricsexplanation.ts
+++ MelodyScout_Bot/botFunctions/callbacks/tracklyricsexplanation.ts
@@ -51,7 +51,7 @@ async function getAiImageByLyrics (lyrics: string, trackName: string, artistName
     }
   }
   const githubApi = new MsGithubApi(githubConfig.token)
-  const uploadToGithub = await githubApi.files.putFile(encodeURI((`(${trackName}):(${artistName}).jpg`).replaceAll(' ', '_')), finalImage.toString('base64'))
+  const uploadToGithub = await githubApi.files.putFile(encodeURI((`[${trackName}]-[${artistName}].jpg`).replaceAll(' ', '_')), finalImage.toString('base64'))
   if (!uploadToGithub.success) {
     return {
       success: false,
