57a8ea3d20ce3dd302438c748b78cd838e1b8737
João Pedro <72423073+JPSAUD501@users.noreply.github.com>
2023-10-06 02:14:29 +0000

    catch

--- api/msLastfmApi/functions/msApiFetch.ts
+++ api/msLastfmApi/functions/msApiFetch.ts
@@ -9,60 +9,72 @@ type MsApiFetchResponse = {
 } | ApiErrors
 
 export const msApiFetch = async (url: string, expectedZodObject: ZodObject<any>): Promise<MsApiFetchResponse> => {
-  const response = await axios.get(url).catch((err: any) => {
-    if (err.response.status === 404) {
-      return err.response.data
+  try {
+    const response = await axios.get(url).catch((err: any) => {
+      if (err.response.status === 404) {
+        return err.response.data
+      }
+      return new Error(err)
+    })
+    if (response instanceof Error) {
+      console.error(response)
+      return {
+        success: false,
+        errorType: 'msApiError',
+        errorData: {
+          message: response.message,
+          error: -1
+        }
+      }
     }
-    return new Error(err)
-  })
-  if (response instanceof Error) {
-    console.error(response)
+    const jsonResponse = response.data
+    if (jsonResponse instanceof Error) {
+      console.error(jsonResponse)
+      return {
+        success: false,
+        errorType: 'msApiError',
+        errorData: {
+          message: jsonResponse.message,
+          error: -2
+        }
+      }
+    }
+    const lfmApiError = zodLfmApiError.safeParse(jsonResponse)
+    if (lfmApiError.success) {
+      console.error(lfmApiError.data)
+      console.error(JSON.stringify(lfmApiError.data, null, 2))
+      return {
+        success: false,
+        errorType: 'lfmApiError',
+        errorData: lfmApiError.data
+      }
+    }
+    const expectedData = expectedZodObject.safeParse(jsonResponse)
+    if (!expectedData.success) {
+      console.error(expectedData.error.issues)
+      console.error(JSON.stringify(expectedData.error.issues, null, 2))
+      return {
+        success: false,
+        errorType: 'msApiError',
+        errorData: {
+          message: expectedData.error.message,
+          error: -3
+        }
+      }
+    }
+    return {
+      success: true,
+      data: expectedData.data
+    }
+  } catch (err) {
+    console.error(String(err))
     return {
       success: false,
       errorType: 'msApiError',
       errorData: {
-        message: response.message,
-        error: -1
+        message: String(err),
+        error: -4
       }
     }
-  }
-  const jsonResponse = response.data
-  if (jsonResponse instanceof Error) {
-    console.error(jsonResponse)
-    return {
-      success: false,
-      errorType: 'msApiError',
-      errorData: {
-        message: jsonResponse.message,
-        error: -2
-      }
-    }
-  }
-  const lfmApiError = zodLfmApiError.safeParse(jsonResponse)
-  if (lfmApiError.success) {
-    console.error(lfmApiError.data)
-    console.error(JSON.stringify(lfmApiError.data, null, 2))
-    return {
-      success: false,
-      errorType: 'lfmApiError',
-      errorData: lfmApiError.data
-    }
-  }
-  const expectedData = expectedZodObject.safeParse(jsonResponse)
-  if (!expectedData.success) {
-    console.error(expectedData.error.issues)
-    console.error(JSON.stringify(expectedData.error.issues, null, 2))
-    return {
-      success: false,
-      errorType: 'msApiError',
-      errorData: {
-        message: expectedData.error.message,
-        error: -3
-      }
-    }
-  }
-  return {
-    success: true,
-    data: expectedData.data
   }
 }
