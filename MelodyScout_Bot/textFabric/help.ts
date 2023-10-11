b32566373c662bea18c29decace639b09fa79146
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2023-10-10 23:46:53 -0300

    Help

--- MelodyScout_Bot/textFabric/help.ts
+++ MelodyScout_Bot/textFabric/help.ts
@@ -1,6 +1,6 @@
 export function getHelpText (ctxLang: string | undefined): string {
   const textArray: string[] = []
-  textArray.push('This function is not implemented yet.')
+  textArray.push('Bem vindo ao MelodyScout! Todos os meus comandos aparecerão ao digitar "/" no chat. Para começar use por exemplo o comando /playingnow para ver o que está ouvindo agora! Se quiser depois gerar uma imagem que represente a musica usando o MelodyScoutAI utilize o botão com o emoji "✨" que irá aparecer!')
   const text = textArray.join('\n')
   return text
 }
