2f7afb5170045530cae858828a48bb30536c9b16
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2023-12-10 14:53:09 -0300

    Add getKandinskyImage function to MsReplicateApi

--- api/msReplicateApi/base.ts
+++ api/msReplicateApi/base.ts
@@ -103,4 +103,85 @@ export class MsReplicateApi {
       }
     }
   }
+
+  async getKandinskyImage (imageDescription: string): Promise<MsReplicateGetSdxlImageResponse> {
+    try {
+      advLog(`MsReplicateApi - getKandinskyImage - imageDescription: ${imageDescription}`)
+      const model = 'ai-forever/kandinsky-2.2:ea1addaab376f4dc227f5368bbd8eff901820fd1cc14ed8cad63b29249e9d463'
+      const output = await this.replicate.run(
+        model,
+        {
+          input: {
+            prompt: imageDescription,
+            negative_prompt: '(worst quality, low quality), tooth, open mouth,bad hand, bad fingers, lowres, bad anatomy, bad hands, error, missing fingers, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, blurry, long neck, out of frame, extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, glitchy, (((long neck))), ((flat chested)), ((((visible hand)))), ((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), extra heads',
+            num_inference_steps: 75,
+            height: 1024,
+            width: 1024
+          }
+        }
+      ).catch((err) => {
+        return new Error(String(err))
+      })
+      if (output instanceof Error) {
+        advError(`MsReplicateApi - getKandinskyImage - Error on running model: ${output.message}`)
+        return {
+          success: false,
+          error: output.message
+        }
+      }
+      const safeParseOutput = z.array(z.string()).safeParse(output)
+      if (!safeParseOutput.success) {
+        advError(`MsReplicateApi - getKandinskyImage - Error on parsing output: ${JSON.stringify(safeParseOutput.error)}`)
+        return {
+          success: false,
+          error: 'Error on parsing output'
+        }
+      }
+      if (safeParseOutput.data.length <= 0) {
+        advError('MsReplicateApi - getKandinskyImage - Output URL not found, length <= 0')
+        return {
+          success: false,
+          error: 'Output URL not found'
+        }
+      }
+      const outputUrl = safeParseOutput.data[0]
+      if (outputUrl.length <= 0) {
+        advError('MsReplicateApi - getKandinskyImage - Output URL not found, length <= 0')
+        return {
+          success: false,
+          error: 'Output URL not found, length <= 0'
+        }
+      }
+      const image = await axios.get(outputUrl, { responseType: 'arraybuffer' }).catch((err) => {
+        return new Error(String(err))
+      })
+      if (image instanceof Error) {
+        advError(`MsReplicateApi - getKandinskyImage - Error on getting image: ${image.message}`)
+        return {
+          success: false,
+          error: image.message
+        }
+      }
+      const imageBufferSafeParse = z.instanceof(Buffer).safeParse(image.data)
+      if (!imageBufferSafeParse.success) {
+        advError(`MsReplicateApi - getKandinskyImage - Error on parsing image buffer: ${JSON.stringify(imageBufferSafeParse.error)}`)
+        return {
+          success: false,
+          error: 'Error on parsing image buffer'
+        }
+      }
+      advLog(`MsReplicateApi - getKandinskyImage - Success! Image URL: ${outputUrl}`)
+      return {
+        success: true,
+        imageUrl: outputUrl,
+        image: imageBufferSafeParse.data
+      }
+    } catch (err) {
+      advError(`MsReplicateApi - getKandinskyImage - Error Try Catch: ${String(err)}`)
+      return {
+        success: false,
+        error: `Error Try Catch: ${String(err)}`
+      }
+    }
+  }
 }
